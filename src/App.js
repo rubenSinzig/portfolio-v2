1d25499220ed6422d232f2fd31fa1bac669d7696
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-17 11:06:51 +0200

    add About to components & sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./components/Nav";
 import Welcome from "./components/Welcome";
+import About from "./components/About";
 
 function App() {
   return (
     <React.Fragment>
       <Nav />
       <Welcome />
+      <About />
     </React.Fragment>
   );
 }
