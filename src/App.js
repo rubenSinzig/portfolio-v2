e01a92e5e727e8a82f4cf7c6200287f57584310e
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-17 11:08:43 +0200

    add Projects to components & sass

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from "./components/Nav";
 import Welcome from "./components/Welcome";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Welcome />
       <About />
+      <Projects />
     </React.Fragment>
   );
 }
