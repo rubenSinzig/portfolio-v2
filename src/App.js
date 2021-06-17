a0dad7007ad58e4d4b6da6af9479e86b753a9751
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-17 11:12:53 +0200

    add Footer to components & sass

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Welcome from "./components/Welcome";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </React.Fragment>
   );
 }
