e2409fc7d1f0109fc53f19685e4c076b7a689da5
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-17 11:10:32 +0200

    add Contact to components & sass

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import Welcome from "./components/Welcome";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Welcome />
       <About />
       <Projects />
+      <Contact />
     </React.Fragment>
   );
 }
