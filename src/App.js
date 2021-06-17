61eda0bfe618aee88fdd9eebd8c8e9c75ab49462
rubenSinzig <ruben.sinzig@posteo.de>
2021-06-17 11:04:05 +0200

    add Welcome to components & sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "./components/Nav";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <React.Fragment>
       <Nav />
+      <Welcome />
     </React.Fragment>
   );
 }
